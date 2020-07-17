a2dba62f7470302e68e3c5dfd31850a7f44660ea
nponcian <51291625+nponcian@users.noreply.github.com>
2020-07-17 21:59:35 +0800

    Add comments to describe the settings parameters

--- doc_reader_python/prepare_settings.py
+++ doc_reader_python/prepare_settings.py
@@ -11,13 +11,18 @@ def prepare_settings(doc_dir, items_per_day=DEFAULT_ITEMS_PER_DAY, separator=NO_
     settings_file = os.path.join(doc_dir, "settings.txt")
 
     with open(settings_file, "w") as settings_file_handler:
-        # TODO : Put this dict into its own class
-        DEFAULT_VALUES = {
+        default_values = {
             "date" : INIT_DATE,
             "line" : INIT_LINE,
             "items_per_day" : items_per_day,
-            "separator" : separator,
-            "mode" : DEFAULT_MODE # possible values # [text, html]
+
+            # The divider between the items in the guidelines. So if each item in the guidelines
+            # are separated by the html tag <h1> Guideline 1... <h1> Guideline 2... and so on, then
+            # you can put here a regex value of "<h[0-9]"
+            "separator" : separator,  # The divider between items in the guidelines. Such as if aaaa
+
+            # Possible values are ["text", "html"]
+            "mode" : DEFAULT_MODE
         }
-        settings_file_handler.write(json.dumps(DEFAULT_VALUES))
+        settings_file_handler.write(json.dumps(default_values))
 
